6f5ddcceec5ed741bb1486c8d87fb7a79528a1bd
xrizwan <xrizwanr@gmail.com>
2020-08-13 11:24:07 -0700

    Updates

---
+++ precache-manifest.ccc754e1e813399b5e4482782d65a166.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28ddc0df0383e0a980a7c62c5eb23150",
+    "url": "/find-waldo/index.html"
+  },
+  {
+    "revision": "0847fd0cf17ffe72a0e4",
+    "url": "/find-waldo/static/css/main.4c2ba895.chunk.css"
+  },
+  {
+    "revision": "aa571860642c6f1db036",
+    "url": "/find-waldo/static/js/2.0d67c13f.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/find-waldo/static/js/2.0d67c13f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0847fd0cf17ffe72a0e4",
+    "url": "/find-waldo/static/js/main.a6b58d33.chunk.js"
+  },
+  {
+    "revision": "55f455ef7472478c690a",
+    "url": "/find-waldo/static/js/runtime-main.514f4642.js"
+  },
+  {
+    "revision": "d8de0a0bc3928088a54338e28ef08144",
+    "url": "/find-waldo/static/media/main.d8de0a0b.jpg"
+  },
+  {
+    "revision": "c8f448a0e3f8c22eae247ed785d54ce6",
+    "url": "/find-waldo/static/media/ss2.c8f448a0.png"
+  },
+  {
+    "revision": "fe5cf257b276f555323db030189299fa",
+    "url": "/find-waldo/static/media/ss3.fe5cf257.png"
+  },
+  {
+    "revision": "2656e386ca880b0280f7332be1d53f12",
+    "url": "/find-waldo/static/media/ss4.2656e386.png"
+  }
+]);
